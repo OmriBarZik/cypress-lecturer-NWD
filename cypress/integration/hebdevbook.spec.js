55acdb8562747bfc4aec0037ddf86aa631eae035
OmriBar-Zik <omri@bar-zik.com>
2019-09-03 22:22:29 +0300

    add: test to check hebdevbook.com when login

--- cypress/integration/hebdevbook.spec.js
+++ cypress/integration/hebdevbook.spec.js
@@ -8,4 +8,26 @@ context('download book button', () => {
   it('test download book button when logout', () => {
     cy.get('.button').contains('התחברו עכשיו')
   })
+
+  
+  it('test download book button when login - manual', () => {
+    cy.get('.button').click();
+    cy.readFile('./Cypress/support/login.json').then( (login) => {
+      cy.get('#user_login').type(login.username)
+      cy.get('#user_pass').type(login.password)
+    })
+    cy.get('#wp-submit').click()
+    cy.visit('https://hebdevbook.com/')
+    cy.get('.button').should('contain','להורדת הספר')
+    .and('have.attr', 'href', '/wp-admin/admin-post.php?action=download_book')
+  })
+
+  it('test download book button when login - request', () => {
+    cy.readFile('./Cypress/support/login.json').then( (login) => {
+      cy.login(login.username, login.password)
+    })
+    cy.reload()
+    cy.get('.button').should('contain','להורדת הספר')
+    .and('have.attr', 'href', '/wp-admin/admin-post.php?action=download_book')
+  })
 })
